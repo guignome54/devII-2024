f462abc073b5bdfcab9a018a0cada4292f678c5c
Dorian-kw <156319237+Dorian-kw@users.noreply.github.com>
2024-11-19 14:48:51 +0100

    Update Event.py (Des details tkt)

--- classes/Event.py
+++ classes/Event.py
@@ -5,8 +5,8 @@ class Event:
     @staticmethod
     def alert(current_capacity, max_capacity):
         """
-        PRE : reçoit la capacité actuelle et maximale du parking
-        POST : si le parking est rempli à 80% ou 95%, affiche une alerte
+        PRE : Reçoit la capacité actuelle et maximale du parking
+        POST : Si le parking est rempli à 80% ou 95%, affiche une alerte
         """
         if current_capacity >= 0.95 * max_capacity:
             print("Alerte : Le parking est presque plein (95%) !")
@@ -15,8 +15,8 @@ class Event:
 
     def change_tarif(self, parking, new_tarif):
         """
-        PRE : reçoit le parking et le nouveau tarif
-        POST : change le tarif actuel du parking
+        PRE : Reçoit le parking et le nouveau tarif
+        POST : Change le tarif actuel du parking
         """
         parking.tarif = new_tarif
         print(f"Le tarif a été mis à jour à {new_tarif}€.")
